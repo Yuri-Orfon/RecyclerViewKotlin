e149eb339e79f332bdde7ba595c2f01c5707f7e4
Yuri Orfon <yurigoncalves1964@hotmail.com>
2020-06-30 23:15:24 -0300

    Ajuste na main e adapter

--- app/src/main/java/com/example/recyclerviewkotlin/presentation/user/Adapter.kt
+++ app/src/main/java/com/example/recyclerviewkotlin/presentation/user/Adapter.kt
@@ -14,21 +14,19 @@ class Adapter (private val userList: List<User>) : RecyclerView.Adapter<Adapter.
         return ViewHolder(v)
     }
 
-    override fun getItemCount(): Int {
-        return userList.size
-    }
+    override fun getItemCount(): Int = userList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val user: User = userList[position]
+        bind(holder, position)
+    }
 
+    private fun bind(holder: ViewHolder, position: Int){
+        val user: User = userList[position]
         holder.textViewName.text = user.name
         holder.textViewOccupation.text = user.occupation
     }
-
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val textViewName = itemView.findViewById(R.id.textviewname) as TextView
         val textViewOccupation = itemView.findViewById(R.id.textviewoccupation) as TextView
-
-
     }
 }
--- app/src/main/java/com/example/recyclerviewkotlin/presentation/user/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewkotlin/presentation/user/MainActivity.kt
@@ -2,21 +2,23 @@ package com.example.recyclerviewkotlin.presentation.user
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProviders.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewkotlin.R
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.Observer
 
 class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeViewModel()
 
+    }
 
-        val viewModel: UserViewModel = ViewModelProviders.of(this).get(UserViewModel::class.java)
+    private fun initializeViewModel() {
+        val viewModel: UserViewModel = of(this).get(UserViewModel::class.java)
         viewModel.usersLiveData.observe(this, androidx.lifecycle.Observer {
             it?.let { user ->
                 with(recyclerview){
